aace7bc9081d971c217bcb291e2bbd78cde59b4c
Ingrid kao <ingrid7613@gmail.com>
2023-05-02 18:09:08 +0800

    1.新增一個nuxt3_typescript+eslint的專案

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [
+        '@nuxtjs/eslint-config-typescript'
+    ],
+    rules: {
+        indent: ['error', 4]
+    }
+}
---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const year = 2023
+const title = `${year} website`
+
+</script>
